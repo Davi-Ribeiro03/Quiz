2bbe35949af4a683bd1b209422bbe3ca91071060
Davi <davi.75.ef@gmail.com>
2024-05-08 16:44:29 -0300

    fix: correção de estilização do background principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <StoreProvider>
       <html lang="en">
         <body
-          className={`${inter.className} bg-main w-full h-full bg-[url("https://i.pinimg.com/originals/53/5c/11/535c1175873becf667d4b630dcf6b343.gif")] bg-no-repeat`}
+          className={`${inter.className} bg-main w-full h-full bg-[url("https://i.pinimg.com/originals/53/5c/11/535c1175873becf667d4b630dcf6b343.gif")]`}
         >
           {/* <Image
             width={250}
